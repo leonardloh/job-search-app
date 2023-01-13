bc7759b1eccc93e1ec40d147fd8fc8953ff56964
Jing Zhi <jing.zhi.loh@intel.com>
2023-01-13 16:15:46 +0800

    init

---
+++ src/playground.js
@@ -0,0 +1,4 @@
+const fruits = ["appple", "banana", "orange"];
+const vegetables = ["cucumber", "radish"];
+
+console.log([...fruits, ...vegetables]);
---
+++ tests/unit/playground.test.js
@@ -0,0 +1,7 @@
+import { describe, test, expect } from "vitest";
+
+describe("basic math", () => {
+  test("adds two numbers", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
