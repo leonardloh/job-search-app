631d19d83248a2b41d7b6427adf52601c21e82e3
Jing Zhi <lohleonard93@gmail.com>
2023-01-23 06:05:17 +0800

    navbar completed

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -4,7 +4,22 @@
       <div
         class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
       >
-        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+        <a :href="url" class="flex h-full items-center text-xl">{{
+          company
+        }}</a>
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li
+              v-for="menuItem in menuItems"
+              :key="menuItem"
+              class="ml-9 h-full first:ml-0"
+            >
+              <a href="" class="flex h-full items-center py-2.5">{{
+                menuItem
+              }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -16,6 +31,15 @@ export default {
   data() {
     return {
       company: "Bobo Careers",
+      url: "https://careers.google.com",
+      menuItems: [
+        "Teams",
+        "Locations",
+        "Life at Bobo Corp",
+        "How we hire",
+        "Students",
+        "Jobs",
+      ],
     };
   },
 };
---
+++ src/playground.js
@@ -0,0 +1,9 @@
+const numbers = [1, 2, 3, 4, 5];
+
+//an array of squares
+
+const squares = numbers.map((n) => {
+  return n * n;
+});
+
+console.log(squares);
